236b62b2c90d88a4fd90cd5488addb460e2f842e
root <root@Ramsay.localdomain>
2019-04-25 22:08:29 -0400

    anything

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,12 +4,24 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open("hashes.txt", "r").read().split('\n')# open and read hashes.txt
+    hashes.pop()
+    passwords_temp = open("passwords.txt", "r").read().split('\n')# open and read passwords.txt
+    passwords_temp.pop()
     characters = string.ascii_lowercase
+    passwords = []
+    for x in passwords_temp:
+        x = x.rstrip()
+        passwords.append(x)
 
     for c in characters:
         for p in passwords:
+            result = c + p
+            for_hash = result.encode("utf-8")
+            encrypt = hashlib.sha256(for_hash).hexdigest()
+            for h in hashes:
+                if encrypt == h:
+                    print(result + ":" + encrypt + "\n")
             # crack hashes
 
             # print hashes as 'input:hash'
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,15 +6,33 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    passwords_temp = open("passwords.txt", "r").read().split('\n')# open and read passwords.txt
+    passwords_temp.pop()
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
-
+    passwords = []
+    for x in passwords_temp:
+        x = x.rstrip()
+        passwords.append(x)
+    server_ip = '134.209.128.58'
+    server_port = 1337
+    counter = 0
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
+
+    while counter < 3:
+        data = s.recv(1024)
+        hashes = data.split('\n')
+        for c in characters:
+            for p in passwords:
+                result = c + p
+                for_hash = result.encode("utf-8")
+                encrypt = hashlib.sha256(for_hash).hexdigest()
+                if encrypt == hashes[2]:
+                    s.send(result + "\n")
+        counter = counter + 1
+
     data = s.recv(1024)
+    print(data)
     # parse data
     # crack 3 times
 
